1387cda96bb62f3190f9023663b1d8de32fbc497
BenSchwartz96 <Elterasvids@gmail.com>
2023-01-10 17:20:26 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,101 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
 
-export default function App() {
+const firebase = require('firebase');
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAtyfLF2fm2dqv2kXZukbp_YWI2DLjZqI",
+  authDomain: "test-30874.firebaseapp.com",
+  projectId: "test-30874",
+  storageBucket: "test-30874.appspot.com",
+  messagingSenderId: "13099037868",
+}
+
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      lists: [],
+    }
+    
+    
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBAtyfLF2fm2dqv2kXZukbp_YWI2DLjZqI",
+      authDomain: "test-30874.firebaseapp.com",
+      projectId: "test-30874",
+      storageBucket: "test-30874.appspot.com",
+      messagingSenderId: "13099037868",
+    }
+
+    if (!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+    }
+    
+    this.referenceShoppingLists = firebase.firestore().collection('shoppinglists');
+    
+  }
+//
+
+
+    componentDidMount() {
+      this.referenceShoppingLists = firebase.firestore().collection('shoppinglists');
+      this.unsubscribe = this.referenceShoppingLists.onSnapshot(this.onCollectionUpdate)
+    }
+    
+     componentWillUnmount() {
+       this.unsubscribe();
+    }
+
+    onCollectionUpdate = (querySnapshot) => {
+      const lists = [];
+      // go through each document
+      querySnapshot.forEach((doc) => {
+        // get the QueryDocumentSnapshot's data
+        var data = doc.data();
+        lists.push({
+          name: data.name,
+          items: data.items.toString(),
+        });
+      });
+      this.setState({
+        lists,
+      });
+    };
+
+
+
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Shopping list data</Text>
+      <FlatList
+        data={this.state.lists}
+        renderItem={({ item }) =>
+        <Text style={styles.item}>{item.name}: {item.items}</Text>}
+      />
     </View>
   );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40,     
   },
+  item: {
+    fontSize: 20,
+    color: 'blue',
+  },
+  text: {
+    fontSize: 30,
+  }
 });
